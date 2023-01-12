95552a24d4b3d6d7958e51e07d602287abbfa21f
AlinaMytsa <act.mitsa@gmail.com>
2023-01-12 22:39:57 +0200

    init

---
+++ my-app/src/components/Definitions.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+class Definitions extends React.Component{
+  render() {
+
+
+    return <dl>
+      <dt>one</dt>
+      <dd>two</dd>
+      <dt>another term</dt>
+      <dd>another description</dd>
+    </dl>
+
+    // <dl>
+    //   <dt>Coffee</dt>
+    //   <dd>Black hot drink</dd>
+    //   <dt>Milk</dt>
+    //   <dd>White cold drink</dd>
+    // </dl>
+  }
+}
+export default Definitions;
