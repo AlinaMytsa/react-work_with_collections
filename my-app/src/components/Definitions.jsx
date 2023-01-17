308ba8b9c1e7ae59a42ea40aa0128bc289ac88be
AlinaMytsa <act.mitsa@gmail.com>
2023-01-17 20:28:52 +0200

    update

--- my-app/src/components/Definitions.jsx
+++ my-app/src/components/Definitions.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+
 class Definitions extends React.Component{
   render() {
+   const {data} = this.props;
 
-
-    return <dl>
-      <dt>one</dt>
-      <dd>two</dd>
-      <dt>another term</dt>
-      <dd>another description</dd>
-    </dl>
-
-    // <dl>
-    //   <dt>Coffee</dt>
-    //   <dd>Black hot drink</dd>
-    //   <dt>Milk</dt>
-    //   <dd>White cold drink</dd>
-    // </dl>
+    return (
+      <dl>{
+        data.map(item => {
+          return(
+            <React.Fragment>
+              <dt key={item.id}>{item.dt}</dt>
+              <dd key={item.id}>{item.dd}</dd>
+            </React.Fragment>
+          )
+        })
+      }
+      </dl>
+    )
   }
 }
 export default Definitions;
